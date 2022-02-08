28e584315a070a390ed95e0d4db44955d760d361
Alex <alex@phone2.im>
2022-02-08 11:28:02 +0300

    test

---
+++ Controllers/VueControllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using EaTS.Data;
+using EaTS.Models;
+using EaTS.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace EaTS.Controllers.VueControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        private readonly ApplicationDbContext _db;
+
+        [HttpPost("/token")]
+        public IActionResult Token(LoginRequest request)
+        {
+            var identity = GetIdentity(request.username, request.password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                accessToken = encodedJwt,
+                username = identity.Name,
+                password = request.password
+            };
+
+            return new ObjectResult(response);
+
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            User user = _db.User.FirstOrDefault(u => u.Login == username && u.Password == password);
+
+            if(user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.ToString())
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+            // если пользователя не найдено
+            return null;
+
+        }
+
+    }
+}
---
+++ Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EaTS.Models
+{
+    public class LoginRequest
+    {
+        public string password { get; set; }
+        public string username { get; set; }
+    }
+}
