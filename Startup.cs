fa32cc62a07762b17cc28ab87784ea36e74806a5
Nicolas <nikolai_vaneev@mail.ru>
2022-02-08 11:44:30 +0300

    Добавлено новое подключение

--- Startup.cs
+++ Startup.cs
@@ -3,16 +3,12 @@ using EaTS.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EaTS
 {
